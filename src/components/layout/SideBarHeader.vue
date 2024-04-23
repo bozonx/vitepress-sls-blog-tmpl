299e0ebf25acf389690ef4b8278c22eb3e5f36a9
Ivan <ipkozyrin@gmail.com>
2024-04-22 23:35:57 -0300

    WIP

--- src/components/layout/SideBarHeader.vue
+++ src/components/layout/SideBarHeader.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { Icon } from '@iconify/vue'
-const props = defineProps(['text', 'href', 'icon', 'class'])
+const props = defineProps(['text', 'href', 'icon', 'mobile', 'class'])
 </script>
 
 <template>
-<h4 class="text-sm pl-2 mt-6 mb-1 text-gray-500 dark:text-gray-400">
+<h4 class="font-bold w-full text-sm pl-3 mt-6 mb-1 text-gray-500 dark:text-gray-400">
   <span v-if="props.icon"><Icon :icon="props.icon" class="text-gray-500 dark:text-gray-400"/></span>
   <span>{{props.text}}</span>
 </h4>

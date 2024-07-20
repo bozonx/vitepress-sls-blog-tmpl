21753cef25622cbb97bd01c19d5019ee7b8f710b
Ivan <ipkozyrin@gmail.com>
2024-07-20 11:04:18 -0300

    WIP

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    // "./src/*.{html,js,vue}",
+    "./src/**/*.{html,js,vue}",
+    // '../vitepress-sls-blog-tmpl/src/**/*.{html,js,vue}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

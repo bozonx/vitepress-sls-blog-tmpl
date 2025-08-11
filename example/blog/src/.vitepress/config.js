c3a6cf79de0329aac398f04fd5c25cd15745434c
Ivan <ipkozyrin@gmail.com>
2025-08-11 15:35:34 -0300

    change

--- example/blog/src/.vitepress/config.js
+++ example/blog/src/.vitepress/config.js
@@ -1,25 +1,28 @@
-import blogConfigBase from "vitepress-sls-blog-tmpl/blogConfigBase.js";
-import { loadBlogLocale } from "vitepress-sls-blog-tmpl/blogConfigHelper.js";
-import { PROPS } from "./props.js";
+import { defineConfig } from 'vitepress'
+import blogConfigBase from 'vitepress-sls-blog-tmpl/blogConfigBase.js'
+import { loadBlogLocale } from 'vitepress-sls-blog-tmpl/blogConfigHelper.js'
 
-const ru = loadBlogLocale("ru", __filename, PROPS);
-const en = loadBlogLocale("en", __filename, PROPS);
-const configBase = blogConfigBase(PROPS, en);
+import { PROPS } from './props.js'
 
-export default {
+const ru = loadBlogLocale('ru', __filename, PROPS)
+const en = loadBlogLocale('en', __filename, PROPS)
+const configBase = blogConfigBase(PROPS, en)
+
+export default defineConfig({
   ...configBase,
   locales: {
     ...configBase.locales,
-    en: { lang: "en-US", ...en },
-    ru: { lang: "ru-RU", ...ru },
+    en: { lang: 'en-US', ...en },
+    ru: { lang: 'ru-RU', ...ru },
   },
   themeConfig: {
     ...configBase.themeConfig,
-    sidebarLogoSrc: "/img/sidebar-logo.webp",
+    sidebarLogoSrc: '/img/sidebar-logo.webp',
+    paginationMaxItems: 7,
   },
   head: [
     ...configBase.head,
     // do not recognize telephone numbers on the page
-    ["meta", { name: "format-detection", content: "telephone=no" }],
+    ['meta', { name: 'format-detection', content: 'telephone=no' }],
   ],
-};
+})
--- example/blog/src/.vitepress/props.js
+++ example/blog/src/.vitepress/props.js
@@ -1,8 +1,7 @@
 export const PROPS = {
-  hostname: "https://",
-  repo: "https://github.com/",
-  siteUrl: "https://",
-  footerCopyright: "Copyright © 2024-present ... .",
+  hostname: 'https://',
+  repo: 'https://github.com/',
+  siteUrl: 'https://',
+  footerCopyright: 'Copyright © 2024-present ... .',
   perPage: 10,
-  paginationMaxItems: 7,
-};
+}

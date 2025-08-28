a2105941a3af6bb61821ca788fadde8606387d91
Ivan <ipkozyrin@gmail.com>
2025-08-28 10:27:58 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 // see https://github.com/vuejs/vitepress/blob/main/src/client/theme-default/styles/components/vp-doc.css

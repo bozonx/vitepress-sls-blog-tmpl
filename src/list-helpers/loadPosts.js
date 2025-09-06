f6e75b56a26697a83674b5ad16ac3ef55e6fca3e
Ivan <ipkozyrin@gmail.com>
2025-09-06 19:55:07 -0300

    change

--- src/list-helpers/loadPosts.js
+++ src/list-helpers/loadPosts.js
@@ -1,15 +1,36 @@
 import fs from 'fs/promises'
 import path from 'path'
 import { makePreviewItem } from './makePreviewItem.js'
+import { POSTS_DIR } from '../constants.js'
+
+if (!global.blogCache) {
+  global.blogCache = {}
+}
 
 /**
  * Загружает все посты из директории postsDir
  *
+ * @param {string} localeDir - Путь к директории локали
  * @param {boolean} ignoreCache - Если true, игнорирует кэш и перечитывает посты
  * @returns {Promise<Array>} Массив обработанных постов
  */
-export async function loadPosts(postsDir) {
+export async function loadPosts(localeDir, ignoreCache = false) {
+  // Извлекаем localIndex из пути (последний элемент пути)
+  const localIndex = path.basename(localeDir)
+
+  // Проверяем глобальный кэш для текущей локали
+  if (
+    global.blogCache[localIndex] &&
+    global.blogCache[localIndex].length > 0 &&
+    !ignoreCache
+  ) {
+    return global.blogCache[localIndex]
+  }
+
   try {
+    // Формируем путь к директории с постами
+    const postsDir = path.join(localeDir, POSTS_DIR)
+
     // Читаем все файлы из директории postsDir
     const files = await fs.readdir(postsDir)
     // Фильтруем только .md файлы
@@ -18,11 +39,13 @@ export async function loadPosts(postsDir) {
     const fullPaths = mdFiles.map((file) => path.join(postsDir, file))
     // Обрабатываем каждый файл через makePreviewItem
     const posts = fullPaths.map((filePath) => makePreviewItem(filePath))
+    // Сохраняем в глобальный кэш для текущей локали
+    global.blogCache[localIndex] = posts
 
     console.log(`\n...Loaded ${posts.length} posts from ${postsDir}`)
 
     return posts
   } catch (error) {
-    throw new Error('Error loading posts:', error)
+    throw new Error(`Error loading posts for locale ${localIndex}:`, error)
   }
 }

f1ae17c64cfe141c23f978a7e905095dbc5b9c02
Ivan <ipkozyrin@gmail.com>
2025-09-06 19:38:40 -0300

    change

--- src/components/list/AllTagsList.vue
+++ src/components/list/AllTagsList.vue
@@ -1,13 +1,9 @@
 <script setup>
 import { makeTagsList } from '../../list-helpers/listHelpers.js'
 import TagsList from '../TagsList.vue'
-import { inject } from 'vue'
 
 const props = defineProps(['allPosts'])
 const tagList = makeTagsList(props.allPosts)
-
-const posts = inject('posts')
-console.log(11111111, posts.value)
 </script>
 
 <template>

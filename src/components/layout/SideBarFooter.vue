034c65c36a775a3a4d5d58e2429a702c7c1d4e5d
Ivan <ipkozyrin@gmail.com>
2025-09-18 14:10:11 -0300

    add

--- src/components/layout/SideBarFooter.vue
+++ src/components/layout/SideBarFooter.vue
@@ -6,34 +6,40 @@ import SwitchAppearance from './SwitchAppearance.vue'
 import SwitchLang from './SwitchLang.vue'
 
 const props = defineProps(['class'])
-const { theme } = useData()
+const { theme, localeIndex } = useData()
 const socialLinks = [
-  ...(theme.value.socialLinks || []),
-  theme.value.sideBar.donate && {
-    text: theme.value.t.links.donate,
-    href: `${theme.value.donate.url}`,
-    icon: theme.value.donate.icon || theme.value.donateIcon,
-    iconClass: 'donate-icon',
+  ...(theme.value.sideBar.socialLinks || []),
+  theme.value.sideBar.rssFeed && {
+    text: theme.value.t.links.rssFeed,
+    href: `/feed-${localeIndex.value}.rss`,
+    target: '_blank',
+    icon: theme.value.rssIcon,
+  },
+  theme.value.sideBar.atomFeed && {
+    text: theme.value.t.links.atomFeed,
+    href: `/feed-${localeIndex.value}.atom`,
+    target: '_blank',
+    icon: theme.value.atomIcon,
   },
 ]
 </script>
 
 <template>
-  <div
-    :class="['w-full flex justify-end pt-10 pb-3 pr-2 space-x-1', props.class]"
-  >
-    <div class="lg:hidden">
-      <SwitchLang @click.stop dropUp="true" noBg="true" />
-    </div>
-
-    <div aria-hidden="true" class="lg:hidden">
-      <SwitchAppearance @click.stop />
-    </div>
-
+  <div :class="['w-full flex pt-6 pb-3 pr-2 pl-2', props.class]">
     <ul v-if="socialLinks.length" class="flex space-x-1">
       <li v-for="item in socialLinks">
         <Btn noBg="true" v-bind="item" />
       </li>
     </ul>
+
+    <div class="flex-1 flex justify-end">
+      <div class="lg:hidden">
+        <SwitchLang @click.stop dropUp="true" noBg="true" />
+      </div>
+
+      <div aria-hidden="true" class="lg:hidden">
+        <SwitchAppearance @click.stop />
+      </div>
+    </div>
   </div>
 </template>

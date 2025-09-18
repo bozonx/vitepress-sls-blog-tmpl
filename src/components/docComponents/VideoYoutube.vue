cdb5be1b73558f6e18ab72e361e28d17107dd045
Ivan <ipkozyrin@gmail.com>
2025-09-18 18:29:57 -0300

    add

---
+++ src/components/docComponents/VideoYoutube.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-if="url" class="video-responsive">
+    <iframe
+      width="100%"
+      height="400"
+      :src="url"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script setup>
+// Got from https://github.com/lando/vitepress-theme-default-plus/blob/main/components/VPLYouTube.vue
+// also see https://github.com/miletorix/vitepress-youtube-embed/blob/main/packages/src/YouTubeEmbed.vue
+
+import { computed } from 'vue'
+
+const props = defineProps({ id: { type: String, required: true } })
+
+const url = computed(() => `https://www.youtube.com/embed/${props.id}`)
+</script>
+
+<style>
+.video-responsive {
+  margin-top: 1em;
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
+}
+.video-responsive iframe {
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+}
+
+@media (max-width: 767px) {
+  .video-responsive {
+    padding-left: 0;
+    padding-right: 0;
+    border-radius: 0;
+    width: auto;
+    margin: 0.85rem -1.5rem;
+    border-radius: 0;
+  }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 // see https://github.com/vuejs/vitepress/blob/main/src/client/theme-default/styles/components/vp-doc.css
 import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css'
-import { YouTubeEmbed } from '@miletorix/vitepress-youtube-embed'
 import FileDownload from './components/docComponents/FileDownload.vue'
 import AudioFile from './components/docComponents/AudioFile.vue'
-import '@miletorix/vitepress-youtube-embed/style.css'
+import VideoYoutube from './components/docComponents/VideoYoutube.vue'
 import './vp-common.css'
 import './vp-icons.css'
 import './styles.css'
@@ -16,8 +15,8 @@ import Layout from './LayoutIndex.vue'
 export default {
   Layout,
   enhanceApp(ctx) {
-    ctx.app.component('YouTubeEmbed', YouTubeEmbed)
     ctx.app.component('FileDownload', FileDownload)
     ctx.app.component('AudioFile', AudioFile)
+    ctx.app.component('VideoYoutube', VideoYoutube)
   },
 }

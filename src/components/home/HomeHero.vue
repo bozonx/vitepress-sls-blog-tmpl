e15dd237a798cb5e10c9c250898efc27de319e25
Ivan <ipkozyrin@gmail.com>
2025-09-25 14:55:16 -0300

    change

--- src/components/home/HomeHero.vue
+++ src/components/home/HomeHero.vue
@@ -1,21 +1,31 @@
 <script setup>
-import Btn from "../Btn.vue";
+import Btn from '../Btn.vue'
 
-const props = defineProps(["firstLine", "secondLine", "buttons", "img"]);
+const props = defineProps(['firstLine', 'secondLine', 'buttons', 'img'])
 </script>
 
 <template>
-  <div class="mb-14 md:mb-24 home-hero">
+  <div class="mb-14 md:mb-18 home-hero">
     <div class="flex max-md:flex-col-reverse">
       <div class="flex-1">
-        <h1 class="max-md:text-4xl md:text-6xl font-bold mb-4" v-html="props.firstLine"></h1>
+        <h1
+          class="max-md:text-4xl md:text-6xl font-bold mb-4"
+          v-html="props.firstLine"
+        ></h1>
         <p class="max-md:text-2xl md:text-4xl" v-html="props.secondLine"></p>
       </div>
-      <div v-if="props.img" aria-hidden="true" class="home-logo md:ml-4 max-md:mb-9">
+      <div
+        v-if="props.img"
+        aria-hidden="true"
+        class="home-logo md:ml-4 max-md:mb-9"
+      >
         <img :src="props.img" />
       </div>
     </div>
-    <ul v-if="props.buttons" class="sm:flex sm:gap-x-3 max-sm:space-y-6 mt-10 home-hero-buttons">
+    <ul
+      v-if="props.buttons"
+      class="sm:flex sm:gap-x-3 max-sm:space-y-6 mt-10 home-hero-buttons"
+    >
       <li v-for="item in props.buttons">
         <Btn v-bind="item" onlyDark="true" class="rounded-full! px-7! w-fit" />
       </li>

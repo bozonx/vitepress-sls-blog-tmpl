7656c26aa7a71e391c4c039548a5ce41bd80eb6c
Ivan <ipkozyrin@gmail.com>
2025-09-26 14:41:49 -0300

    add

--- src/components/list/PopularPostsList.vue
+++ src/components/list/PopularPostsList.vue
@@ -12,16 +12,35 @@ const sortBy = theme.value.popularPosts?.sortBy || 'pageviews'
 const posts = [...(props.allPosts || [])]
   // .filter((post) => post.analyticsStats) // Только посты с данными аналитики
   .sort((a, b) => {
-    const aValue = a.analyticsStats?.[sortBy] || 0
-    const bValue = b.analyticsStats?.[sortBy] || 0
+    const aHasStats =
+      a.analyticsStats?.[sortBy] !== undefined &&
+      a.analyticsStats?.[sortBy] !== null
+    const bHasStats =
+      b.analyticsStats?.[sortBy] !== undefined &&
+      b.analyticsStats?.[sortBy] !== null
 
-    // Для bounceRate сортируем по возрастанию (меньше = лучше)
-    if (sortBy === 'bounceRate') {
-      return aValue - bValue
+    // Если у обоих постов есть статистика, сортируем по ней
+    if (aHasStats && bHasStats) {
+      const aValue = a.analyticsStats[sortBy]
+      const bValue = b.analyticsStats[sortBy]
+
+      // Для bounceRate сортируем по возрастанию (меньше = лучше)
+      if (sortBy === 'bounceRate') {
+        return aValue - bValue
+      }
+
+      // Для остальных метрик сортируем по убыванию (больше = лучше)
+      return bValue - aValue
     }
 
-    // Для остальных метрик сортируем по убыванию (больше = лучше)
-    return bValue - aValue
+    // Если у одного есть статистика, а у другого нет - приоритет у того, у кого есть
+    if (aHasStats && !bHasStats) return -1
+    if (!aHasStats && bHasStats) return 1
+
+    // Если у обоих нет статистики, сортируем по дате (новые сначала)
+    const aDate = new Date(a.date || 0)
+    const bDate = new Date(b.date || 0)
+    return bDate - aDate
   })
   .slice(0, theme.value.perPage)
 </script>

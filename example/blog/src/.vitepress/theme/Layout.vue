06b53d9d3fdc4e6343a749429efa6ecfb69c95e7
Ivan <ipkozyrin@gmail.com>
2025-09-30 13:17:15 -0300

    change

--- example/blog/src/.vitepress/theme/Layout.vue
+++ example/blog/src/.vitepress/theme/Layout.vue
@@ -1,20 +1,11 @@
 <script setup>
 import Theme from 'vitepress-sls-blog-tmpl'
 import { provide } from 'vue'
-import { data as ruData } from '../../ru/loadPosts.data.js'
 import { data as enData } from '../../en/loadPosts.data.js'
-import { data as ruPopularPostsData } from '../../ru/loadPopularPosts.data.js'
-import { data as enPopularPostsData } from '../../en/loadPopularPosts.data.js'
 
-const posts = { ru: ruData.posts, en: enData.posts }
-
-const popularPosts = {
-  ru: ruPopularPostsData.posts,
-  en: enPopularPostsData.posts,
-}
+const posts = { en: enData.posts }
 
 provide('posts', posts)
-provide('popularPosts', popularPosts)
 
 const { Layout } = Theme
 </script>
